977a6b1eb50980fe92bbe31fec6b1006e5692fbc
Mohammad Khalilzadeh <140876993+MuhammadKhalilzadeh@users.noreply.github.com>
2026-05-19 20:52:18 +0330

    IP Address

--- App/lib/core/constants/api_constants.dart
+++ App/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://10.0.2.2:3000'; // Android emulator localhost
+  static const String baseUrl = 'http://192.168.1.105:3000'; // Android emulator localhost
   static const Duration connectTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
 }
